c7395f06adb191386109a9c42abbcf5b76fbde48
Julie Han <juliemenghan@gmail.com>
2022-08-16 14:48:26 -0400

    step1 done and tested

---
+++ step1.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const fsP = require("fs/promises");
+
+const argv = process.argv;
+
+/** Reads the contents of a file at the given path and prints the contents
+ * else throws an error
+ *
+ * @param {*} path
+ */
+
+async function cat(path) {
+  path = argv[2];
+  try {
+    const contents = await fsP.readFile(path, "utf8");
+    console.log("path contents", contents);
+  }
+  catch (e) {
+    console.error("error", e);
+    process.exit(1);
+  }
+}
+
+cat();
